e396bc56f2ed70f3cb2dd596c484151c5f51aadf
Eric Yankowitz <trielore@gmail.com>
2019-10-06 18:08:10 -0400

    answer

--- linkedlist.c
+++ linkedlist.c
@@ -16,48 +16,108 @@ void delete_linkedlist(linkedlist *l)
 }
 int size_linkedlist(linkedlist *l)
 {
-	return 0;
+    int sz = 0;
+    node* it = l->head;
+    while (it) {
+        ++sz;
+        it = it->next;
+    }
+    return sz;
 }
 void insert_front(linkedlist *l, int value)
 {
-}
+    node* nh = (node*)malloc(sizeof(node));
+    nh->next = l->head;
+    nh->value = value;
+    l->head = nh;
+};
 void insert_back(linkedlist *l, int value)
 {
-}
-node* get_head_node(linkedlist* l) 
-{
-   return 0;
+    node* it = l->head;
+    if (it == 0) {
+        insert_front(l, value);
+    }
+    while (it->next) {
+        it = it->next;
+    }
+    node* nh = (node*)malloc(sizeof(node));
+    nh->next = 0;
+    nh->value = value;
+    it->next = nh;
 }
 
-int get_node_value(node* v) 
+node* get_head_node(linkedlist* l) 
 {
-	return -1;
+    return l->head;
+}
+
+int get_node_value(node* v) {
+    return v->value;
 }
 
 node* get_next(node* it)
 {
-    return 0;
+    return it->next;
 }
 void insert_after(node* it, int value)
 {
+    node* nh = (node*)malloc(sizeof(node));
+    nh->next = it->next;
+    nh->value = value;
+    it->next = nh; 
 }
 
 void replace_value_with(node* n, int new_value) {
+    n->value = new_value;
 }
 
 boolean get_head_value(linkedlist *l, int *v)
 {
-    return FALSE;
+    if (!l->head)
+        return FALSE;
+    *v = l->head->value;
+    return TRUE;
 }
 boolean get_tail_value(linkedlist *l, int *v)
 {
-    return FALSE;
+    node* it = l->head;
+    if (it == 0) {
+        return FALSE;
+    }
+    while (it->next) {
+        it = it->next;
+    }
+    *v = it->value;
+    return TRUE;
 }
 boolean remove_head(linkedlist *l, int *v)
 {
-    return FALSE;
+    node* it = l->head;
+    if (!it)
+        return FALSE;
+    *v = it->value;
+    l->head = it->next;
+    free(it);
+    return TRUE;
 }
 boolean remove_tail(linkedlist *l, int *v)
 {
-    return FALSE;
+    node* it = l->head;
+    if (it == 0) {
+        return FALSE;
+    }
+    node* pit = it;
+    while (it->next) {
+        pit = it;
+        it = it->next;
+    }
+    if (pit == it) {
+        //we are deleting the last element in the list
+        l->head = 0;
+    } else {
+        pit->next = 0;
+    }
+    *v = it->value;
+    free(it);
+    return TRUE;
 }
